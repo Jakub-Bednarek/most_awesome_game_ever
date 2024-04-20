70dfbc1e0e76731a08e3d32eee1ce29273ae5197
jakub.bednarek <jakub.bednare1009@gmail.com>
2024-04-20 21:20:03 +0200

    Improved spawning mechanism, changed behaviour to spawn only when player is on eligible level

--- Assets/_Scripts/SpawnEnemies.cs
+++ Assets/_Scripts/SpawnEnemies.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class SpawnEnemies : MonoBehaviour
 {
     [SerializeField] List<FollowPlayer> enemiesToSpawn;
+    [SerializeField] FollowPlayer spawnedMonster = null;
     [SerializeField] bool isMonsterSpawned = false;
     [SerializeField] float monsterSpawnChangeInPercent = 25.0f;
     private Player player;
     private float delay = 3.0f;
     private float timePassed = 0.0f;
+    [SerializeField] bool isLevelEligibleForSpawningMonsters = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,24 +22,60 @@ public class SpawnEnemies : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(!isLevelEligibleForSpawningMonsters)
+        {
+            return;
+        }
+
         timePassed += Time.deltaTime;
-        if(ShouldMonsterBeSpawned() && !isMonsterSpawned)
+        if(ShouldMonsterBeSpawned())
         {
             SpawnRandomMonster();
         }
     }
 
+    private void OnLevelChange(int level)
+    {
+        if(level == 0)
+        {
+            isLevelEligibleForSpawningMonsters = false;
+            ResetPropertiesOnSafeLevelChange();
+        }
+        else
+        {
+            isLevelEligibleForSpawningMonsters = true;
+        }
+    }
+
+    private void ResetPropertiesOnSafeLevelChange()
+    {
+        timePassed = 0.0f;
+
+        if(spawnedMonster != null)
+        {
+            Destroy(spawnedMonster.gameObject);
+            isMonsterSpawned = false;
+        }
+    }
+
     private bool ShouldMonsterBeSpawned()
     {
-        if (timePassed > delay)
+        if (timePassed < delay)
         {
-            SpawnRandomMonster();
-            timePassed -= delay;
-
-            return Random.Range(0, 100) > monsterSpawnChangeInPercent;
+            return false;
         }
 
-        return false;
+        timePassed -= delay;
+        return !isMonsterSpawned && Random.Range(0, 100) < monsterSpawnChangeInPercent;
+
+    }
+
+    private Vector3 GenerateRandomMonsterPosition()
+    {
+        float xOffset = Random.Range(8.0f, 30.0f); 
+        float zOffset = Random.Range(8.0f, 30.0f); 
+
+        return player.transform.position + new Vector3(xOffset, 0.0f, zOffset);
     }
 
     private void SpawnRandomMonster()
@@ -46,7 +84,7 @@ public class SpawnEnemies : MonoBehaviour
         {
             isMonsterSpawned = true;
             int monsterIndexToSpawn = Random.Range(0, enemiesToSpawn.Count);
-            Instantiate(enemiesToSpawn[monsterIndexToSpawn], player.transform.position, Quaternion.identity);
+            Instantiate(enemiesToSpawn[monsterIndexToSpawn], GenerateRandomMonsterPosition(), Quaternion.identity);
         }
     }
 }
